88fad3c402d9e657d35720d163c7cbf7673d341b
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-13 18:03:37 +0530

    Create Customer2.java

---
+++ Customer2.java
@@ -0,0 +1,53 @@
+/* static synchronization */
+class Bank extends Thread
+{
+    static int bal=5000;
+    static int withdraw;
+
+    Bank(int withdraw)
+    {
+        this.withdraw=withdraw; // withdraw=5000
+    }
+    public static synchronized void Withdraw()  // t1 t2 t3 t4
+    {
+        String name=Thread.currentThread().getName();
+        if(withdraw<=bal)  // 5000<=5000
+        {
+            System.out.println(name+" withdrawn money");
+            bal=bal-withdraw;  // bal=5000-5000=0
+        }
+        else
+        {
+            System.out.println("Insufficient Balance");
+        }
+    }
+    public void run()
+    {
+        Withdraw();
+    }
+}
+class Customer // bank has one lock
+{
+    public static void main(String[] args) {
+        
+        Bank obj=new Bank(5000);  
+
+        Thread t1=new Thread(obj);
+        Thread t2=new Thread(obj);
+
+        t1.setName("Raju");
+        t2.setName("Sunita");
+
+        Bank obj2=new Bank(5000); 
+
+        Thread t3=new Thread(obj2);
+        Thread t4=new Thread(obj2);
+
+        t3.setName("Rahul");
+        t4.setName("Anita");
+
+
+        t1.start();  t2.start();
+        t3.start();  t4.start();
+    }
+}
